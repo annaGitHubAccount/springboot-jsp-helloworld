d27ba3a4c3006f386793e6f6983a8e42bc2565be
annaGitHubAccount <Rusaczek123>
2019-07-11 09:58:59 +0200

    KundeService

--- src/main/java/com/hellokoding/springboot/app/repository/KundeRepository.java
+++ src/main/java/com/hellokoding/springboot/app/repository/KundeRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 // przy CrudRepository nie musze juz robic implementacji, wystarczy sam interfejs
 // w Repository nazywam tylko odpowiednio metody i Spring sam wykonuje odpowiednie operacje w bazie danych na podstawie nazwy metody
 
+// nie moge tu pisac metody np: void save(Kunde kunde); bo CrudRepository posiada juz taka metode. Jak to zrobie, to nie zaladuje sie spring context i dostane Exception
+// tutaj zawieram tylko te metody, ktorych nie ma w CrudRepository.
 
 
 public interface KundeRepository extends CrudRepository<Kunde, Long> {
